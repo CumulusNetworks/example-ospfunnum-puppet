ba93d12a8d15d0a6b09d9c8df0d20ae8f525897c
Nat Morris <nat@nuqe.net>
2015-04-01 09:16:35 -0400

    notes in 2s manifest

---
+++ manifests/site-2s.pp
@@ -0,0 +1,9 @@
+# license using our module (via librarian)
+
+# ports if need be using our module (via librarian)
+
+# interfaces using out module (via librarian)
+
+# ptm
+
+# quagga using module in this repo
