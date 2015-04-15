f3d096343dbce597a5473474cc637a0620122061
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-04-15 12:01:55 +0100

    Import Cumulus manifests from cldemo
    Import the existing Puppet manaifests from the cldemo packages.
    Update the librarian metadata file to pull in the 3rd party stdlib, sysctl &
    account manifests from upstream.

---
+++ modules/monitoring/files/ganglia.conf.php
@@ -0,0 +1,8 @@
+<?php
+$conf['gweb_confdir'] = "/var/lib/ganglia-web";
+$conf['gmetad_root'] = "/var/lib/ganglia";
+$conf['rrds'] = "${conf['gmetad_root']}/rrds";
+$conf['dwoo_compiled_dir'] = "${conf['gweb_confdir']}/dwoo/compiled";
+$conf['dwoo_cache_dir'] = "${conf['gweb_confdir']}/dwoo/cache";
+$conf['cachedata'] = 0;
+?>
