2e5475db16de19d3a155936ccf04d618d95597b9
Kristian Van Der Vliet <kristian@cumulusnetworks.com>
2015-06-18 16:23:37 +0100

    Use Cumulus modules
    Install the Cumulus modules from Forge using Librarian.
    Use the cumulus_license module to install the license.

--- modules/base/manifests/license.pp
+++ modules/base/manifests/license.pp
@@ -1,21 +1,15 @@
 class base::license {
-    service { 'switchd':
-          ensure     => running,
-          hasstatus  => true,
-          hasrestart => true,
-          enable     => true
-    }
 
-    file { '/etc/cumulus/license.txt':
-        owner  => root,
-        group  => root,
-        mode   => '0644',
-        source => "puppet:///modules/base/${::hostname}.lic",
-        notify => Service['switchd']
-    }
+  service { 'switchd':
+    ensure     => running,
+    hasstatus  => true,
+    hasrestart => true,
+    enable     => true
+  }
 
-    exec { '/usr/cumulus/bin/cl-license -i /etc/cumulus/license.txt; service switchd restart':
-        subscribe   => File['/etc/cumulus/license.txt'],
-        refreshonly => true
-    }
+  cumulus_license { 'workbench':
+    src => "puppet:///modules/base/${::hostname}.lic",
+    notify => Service['switchd']
+  }
+
 }
