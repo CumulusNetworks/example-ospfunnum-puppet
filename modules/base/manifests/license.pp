e7d83ead2e62f703ca71cafccfa0d0651704af3a
Leslie Carr <geekgirl@gmail.com>
2015-07-30 18:15:26 -0700

    copying local license file for installation

--- modules/base/manifests/license.pp
+++ modules/base/manifests/license.pp
@@ -7,8 +7,16 @@ class base::license {
     enable     => true
   }
 
+# cumulus_license requires a web address or local file
+  file { "/tmp/${::hostname}.lic":
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    source => "puppet:///modules/base/${::hostname}.lic"
+  }
+
   cumulus_license { 'workbench':
-    src => "puppet:///modules/base/${::hostname}.lic",
+    src    => "puppet:///modules/base/${::hostname}.lic",
     notify => Service['switchd']
   }
 
